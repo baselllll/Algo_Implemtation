98c85db0810c946c5e788db7af10501bce093e3a
Basel osama ibrahim Ahmed <48773708+baselllll@users.noreply.github.com>
2022-06-20 10:21:47 +0200

    Time: 576 ms (67.37%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -1,9 +1,2 @@
-
-
-SELECT employee_id,
-    CASE 
-        WHEN employee_id = 2 THEN 0
-        WHEN (MOD(employee_id,2) = 1) and (substring(name,1,1) <> 'M') THEN salary
-        ELSE 0
-    END as bonus
-FROM Employees
+select employee_id, case when employee_id %2 <> 0 and substr(name,1,1) <> 'M' then salary else
+0 end as bonus from employees order by employee_id
