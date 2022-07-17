e016608f03080fa384a927e2c5300232afe452af
Basel osama ibrahim Ahmed <48773708+baselllll@users.noreply.github.com>
2022-07-17 11:58:51 +0200

    Time: 11 ms (77.09%), Space: 19.5 MB (55.37%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.php
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.php
@@ -6,7 +6,7 @@ class Solution {
      */
     function runningSum($nums) {
         $arr = array($nums[0]);
-        for ($i=0; $i < count($nums)-1; $i++) { 
+        for ($i=0; $i < sizeof($nums)-1; $i++) { 
             $new_item = $nums[$i+1] = $nums[$i]+$nums[$i+1];
             array_push($arr, $new_item);
         }
