ec2e6b4e5475446de13803484a9b8febbf6cea6a
Basel osama ibrahim Ahmed <48773708+baselllll@users.noreply.github.com>
2022-10-13 23:12:07 +0200

    Create queue_Implement_using_LinkedList.js

---
+++ queue_Implement_using_LinkedList.js
@@ -0,0 +1,58 @@
+
+// implemntion the stack with Array
+
+/*
+css
+udemy
+js
+*/
+class Node {
+    constructor(value) {
+        return this.head = {
+            value: value,
+            next: null
+        }
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null
+        this.last = null
+        this.length = 0
+    }
+    peek() {
+        return this.first;
+    }
+    enqueue(value) {
+        let newNode = new Node(value)
+        if (this.length === 0) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode
+        }
+        this.length++
+        return this
+
+    }
+    dequeue() {
+        if (!this.first) {
+            return null
+        }
+        if (this.first === this.last) {
+            this.last = null
+        }
+        this.first = this.first.next
+        this.length--;
+        return this
+    }
+}
+
+
+const queue = new Queue();
+queue.enqueue('udemy')
+queue.enqueue('css')
+queue.dequeue()
+console.log(queue)
