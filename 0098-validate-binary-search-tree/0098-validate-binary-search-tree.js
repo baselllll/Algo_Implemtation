00a35e9aead1b365b012b3d9d41ebcba6f6c3ae4
Basel osama ibrahim Ahmed <48773708+baselllll@users.noreply.github.com>
2022-10-26 21:28:43 +0200

    Time: 114 ms (56.12%), Space: 46.3 MB (60.12%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -10,17 +10,12 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isValidBST = function(root) {
-     if(!root) 
-        return true
-    return dfs(root, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)
-    
-    function dfs(root, min, max){
-        console.log(root+" "+min+" "+max)
-        if(!root) 
-            return true
-        if(root.val <= min || root.val >= max)
-            return false
-        return dfs(root.left, min, root.val) && dfs(root.right, root.val, max)
-    }
+var isValidBST = function(root, min, max){
+    return isValidBSTHelper(root, -Infinity, Infinity);
 };
+
+let isValidBSTHelper = function(root, min, max){
+    if(root === null) return true;
+    if(root.val <= min || root.val >= max) return false;
+    return isValidBSTHelper(root.left, min, root.val) && isValidBSTHelper(root.right, root.val, max);
+}
