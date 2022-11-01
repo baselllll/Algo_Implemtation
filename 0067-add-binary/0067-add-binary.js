a80323d36c0dcc00285d27e1efef05942faf6b73
Basel osama ibrahim Ahmed <48773708+baselllll@users.noreply.github.com>
2022-11-01 13:07:42 +0200

    Time: 109 ms (48.79%), Space: 42 MB (96.49%) - LeetHub

--- 0067-add-binary/0067-add-binary.js
+++ 0067-add-binary/0067-add-binary.js
@@ -4,10 +4,11 @@
  * @return {string}
  */
 var addBinary = function(a, b) {
-    const decimalA =  `0b${a}`
-     const decimalB = `0b${b}`
-     const addition = BigInt(decimalA) + BigInt(decimalB)
-     const res  = addition.toString(2)
-     return res
+    // const decimalA =  `0b${a}`
+    //  const decimalB = `0b${b}`
+    //  const addition = BigInt(decimalA) + BigInt(decimalB)
+    //  const res  = addition.toString(2)
+    //  return res
+     return (BigInt(`0b${a}`)+BigInt(`0b${b}`)).toString(2)
     
 };
