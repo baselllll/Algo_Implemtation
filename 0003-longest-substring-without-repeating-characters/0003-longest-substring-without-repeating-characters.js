434cb43e21bf3ec1ea3531978974956d96e82986
Basel osama ibrahim Ahmed <48773708+baselllll@users.noreply.github.com>
2022-11-13 11:16:44 +0200

    Time: 122 ms (83.91%), Space: 44.8 MB (94.26%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,18 +3,30 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-  var k = 0;
-    var maxLength = 0;
-    for(i = 0; i < s.length; i++) {
-        for (j = k; j < i; j++) {
-            if (s[i] === s[j]) {
-                k = j + 1;
-                break;
-            }
+  // var k = 0;
+  //   var maxLength = 0;
+  //   for(i = 0; i < s.length; i++) {
+  //       for (j = k; j < i; j++) {
+  //           if (s[i] === s[j]) {
+  //               k = j + 1;
+  //               break;
+  //           }
+  //       }
+  //       if (i - k + 1 > maxLength) {
+  //           maxLength = i - k + 1;
+  //       }
+  //   }
+  //   return maxLength; 
+    var start = 0, maxLen = 0;
+    var map = new Map();
+    for (let index = 0; index < s.length; index++) {
+        let ch = s[index]
+        // check that char exist on Map -----> do update for the start (that mean that exist repition)
+        if (map.get(ch) >= start) {
+            start = map.get(ch) + 1;
         }
-        if (i - k + 1 > maxLength) {
-            maxLength = i - k + 1;
-        }
+        map.set(ch, index)
+        maxLen = Math.max(maxLen, index - start + 1)
     }
-    return maxLength;  
+    return maxLen
 };
